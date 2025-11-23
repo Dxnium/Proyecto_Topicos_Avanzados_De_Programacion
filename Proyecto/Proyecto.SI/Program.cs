71d2bb9ad33ca163f7084437e9a801df7ad5dc0f
unknown <danielum99@gmail.com>
2025-11-23 15:59:37 -0600

    enable swagger

--- Proyecto/Proyecto.SI/Program.cs
+++ Proyecto/Proyecto.SI/Program.cs
@@ -24,11 +24,9 @@ builder.Services.AddScoped<GestionDePersonas.BL.IVehiculoRepository, VehiculoRep
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
